d8f712611aa30e6e9466f8cc006472c214c3722e
juanlll <juan.vargasva@gmail.com>
2022-12-01 17:59:06 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 function generateHashForTable(key = '', i = '-') {
-  return Buffer.from(new String(key).valueOf())
-    .toString('base64')
-    .replace(/(.{7})/g, '$1' + i)
-    .trim();
+  return Buffer.from(new String(key).valueOf()).toString('base64').trim();
 }
